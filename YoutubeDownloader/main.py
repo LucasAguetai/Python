1d8dfac97a1ce4e477ce30fd8ae07ed8e826df76
LucasAguetai <lucas.aguetai@icloud.com>
2022-01-28 15:58:58 +0100

    finish

--- YoutubeDownloader/main.py
+++ YoutubeDownloader/main.py
@@ -4,13 +4,21 @@ from pytube import YouTube
 
 url = input("Quel est le lien de la vidéo ? : ")
 yt = YouTube(url)
+validation = "nothing"
 
 #I request verification that it is the right video 
 
-print("Est-ce bien cette vidéo :", yt.title, "(y or n)")
-validation = input()
-if validation == "y" or validation == "Y" :
-	#I get the best quality possible
-	yt = yt.streams.get_highest_resolution()
-	#I download
-	yt.download()
+while validation != "" :
+	print("Est-ce bien cette vidéo :", yt.title, "(y or n)")
+	validation = input()
+	if validation == "y" or validation == "Y" :
+		#I get the best quality possible
+		yt = yt.streams.get_highest_resolution()
+		#I download
+		yt.download()
+		print("Le téléchargement est terminé ! Bon visionnage :D")
+		validation = ""
+	else :
+		print("Recommençons")
+		url = input("Quel est le lien de la vidéo ? : ")
+		yt = YouTube(url)
