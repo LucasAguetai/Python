d2b35fbab054e50b84a293daba7fb9dbc3e3df55
LucasAguetai <lucas.aguetai@icloud.com>
2022-02-07 11:18:35 +0100

    Tic-Tac-Toe done

--- Tic-Tac-Toe/main.py
+++ Tic-Tac-Toe/main.py
@@ -1,6 +1,12 @@
 from random import *
 
 mode = int(input("1 : 1 joueur\n2 : 2 jouers\n"))
+if mode == 2 :
+	player1 = input("Quelle est le nom du joueur 1 ? (X) ")
+	player2 = input("Quelle est le nom du joueur 2 ? (O) ")
+else :
+	player1 = input("Quelle est votre nom ? ")
+	player2 = "L'ordinateur"
 
 def game(plateau, case, symbole) :
 	new_plateau = ""
@@ -15,30 +21,38 @@ def game(plateau, case, symbole) :
 
 def win(plateau, case) :
 	res = 0
-	if not (plateau[1] == "case" and plateau[3] == "case" and plateau[5] == "case") :
+	if not (plateau[0] == case and plateau[2] == case and plateau[4] == case) :
 		res += 1
-		if not (plateau[1] == "case" and plateau[13] == "case" and plateau[25] == "case") :
+		if not (plateau[0] == case and plateau[12] == case and plateau[24] == case) :
 			res += 1
-			if not (plateau[1] == "case" and plateau[15] == "case" and plateau[29] == "case") :
+			if not (plateau[0] == case and plateau[14] == case and plateau[28] == case) :
 				res += 1
-				if not (plateau[5] == "case" and plateau[13] == "case" and plateau[25] == "case") :
+				if not (plateau[4] == case and plateau[12] == case and plateau[24] == case) :
 					res += 1
-					if not (plateau[5] == "case" and plateau[17] == "case" and plateau[29] == "case") :
+					if not (plateau[4] == case and plateau[16] == case and plateau[28] == case) :
 						res += 1
-						if not (plateau[25] == "case" and plateau[27] == "case" and plateau[29] == "case") :
+						if not (plateau[24] == case and plateau[26] == case and plateau[28] == case) :
 							res += 1
-	if res == 6 :
+							if not (plateau[2] == case and plateau[14] == case and plateau[26] == case) :
+								res += 1
+	if res == 7 :
 		return False
 	else :
 		return True
 
 plateau = "1|2|3\n- - -\n4|5|6\n- - -\n7|8|9"
-
-x=input("Voulez vous commencez ? (o or n)\n")
-if x == "o" or x == "O" :
-	player = 1
+if mode == 1 :
+	x=input("Voulez vous commencez ? (o or n)\n")
+	if x == "o" or x == "O" :
+		player = 1
+	else :
+		player = 2
 else :
-	player = 2
+	x=input(player1 + ", voulez vous commencez ? (o or n)\n")
+	if x == "o" or x == "O" :
+		player = 1
+	else :
+		player = 2
 
 rounds = 0
 end = ""
@@ -57,13 +71,27 @@ while end != "end" :
 			player = 1
 		rounds += 1
 
-		if rounds == 9 :
-			print(plateau + "\nÉgalité !")
-			end = "end"
+	elif mode == 2 :
+		if player == 1 :
+			print(player1 + ", quelle case voulez-vous jouez ?\n")
+			case = input(plateau + "\n")
+			plateau = game(plateau, case, "X")
+			player = 2
+		else :
+			print(player2 + ", quelle case voulez-vous jouez ?\n")
+			case = input(plateau + "\n")
+			plateau = game(plateau, case, "O")
+			player = 1
+		rounds += 1
 
-		if win(plateau, "O") :
-			print("L'ordinateur a gagné !")
-			end = "end"
-		elif win(plateau, "X") :
-			print("Vous avez gagnez !")
-			end = "end"
+	if rounds == 9 :
+		print(plateau + "\nÉgalité !")
+		end = "end"
+
+	if win(plateau, "O") :
+		print(plateau)
+		print(player2 + " a gagné !")
+		end = "end"
+	elif win(plateau, "X") :
+		print(player1 + " a gagnez !")
+		end = "end"
